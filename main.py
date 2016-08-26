c5fd9b812e668f49ade891c38f40c62c0fe0f8b6
Xuan Yan <xuanyan@xuanyan.ws>
2016-08-26 10:27:31 +0800

    support searching fractional targets

--- main.py
+++ main.py
@@ -2,24 +2,27 @@
 
 import sys
 import re
+from fractions import Fraction
 from tchisla import Tchisla
 
-number_re = re.compile("^\\d+$")
-tchisla_re = re.compile("^\\d+#\\d$")
+number_re = re.compile("^\\d+(/\\d+)?$")
+tchisla_re = re.compile("^\\d+(/\\d+)?#\\d$")
 
 def problem_printer(n, target):
     print("{target} # {n}".format(target = target, n = n))
 
 def solve(string):
     if number_re.match(string):
-        target = int(string)
+        target = Fraction(string)
         for n in range(1, 10):
             problem_printer(n, target)
             tchisla = Tchisla(n, target)
             tchisla.solve()
             tchisla.print_solution(target, True)
     elif tchisla_re.match(string):
-        target, n = tuple(map(int, string.split("#")))
+        array = string.split("#")
+        target = Fraction(array[0])
+        n = int(array[1])
         problem_printer(n, target)
         tchisla = Tchisla(n, target)
         tchisla.solve()
