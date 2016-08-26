bc96fdecc71789f9054dab6dba1185db9e97f306
Xuan Yan <xuanyan@xuanyan.ws>
2016-08-26 08:05:47 +0800

    Expression

---
+++ expression.py
@@ -0,0 +1,22 @@
+__all__ = ["Expression"]
+
+class Expression:
+    __slots__ = ("name", "args")
+
+    def __init__(self, name = None, *args):
+        self.name = name
+        self.args = tuple(args)
+
+    def __str__(self):
+        if self.args == ():
+            return ""
+        elif len(self.args) == 1:
+            arg = str(self.args[0])
+            if self.name == "sqrt":
+                return "sqrt(" + arg + ")"
+            elif self.name == "factorial":
+                return arg + "!"
+            elif self.name == "-":
+                return "-" + arg
+        else:
+            return (" " + self.name + " ").join(map(str, self.args))
