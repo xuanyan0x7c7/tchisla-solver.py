95492e1443113d51ddd7fd5de3fbfa023a0a2c44
Xuan Yan <xuanyan@xuanyan.ws>
2016-08-29 08:24:07 +0800

    greatly improve code

--- main.py
+++ main.py
@@ -8,65 +8,54 @@ from solver.rational import RationalTchisla
 
 integer_re = re.compile("^\\d+")
 rational_re = re.compile("^\\d+(/\\d+)?$")
+number_re = rational_re
 
-def integer_solver(n, target):
-    if not integer_re.match(target):
-        return
-    target = int(target)
-    print(str(target) + " # " + str(n))
-    tchisla = IntegerTchisla(n, target)
-    tchisla.solve()
-    for string in tchisla.solution_prettyprint(target, True):
-        print(string)
-
-def rational_solver(n, target):
-    if not rational_re.match(target):
-        return
-    target = Fraction(target)
-    print(str(target) + " # " + str(n))
-    tchisla = RationalTchisla(n, target)
-    tchisla.solve()
-    for string in tchisla.solution_prettyprint(target, True):
-        print(string)
-
-def incremental_solver(n, target):
-    if not rational_re.match(target):
-        return
-    target = Fraction(target)
-    print(str(target) + " # " + str(n))
-    max_depth = None
-    solution_list = []
-    if target.denominator == 1:
-        integer_target = int(target)
-        tchisla = IntegerTchisla(n, integer_target)
-        max_depth = tchisla.solve()
-        solution_list = tchisla.solution_prettyprint(integer_target, True)
-        for string in solution_list:
-            print(string)
-    tchisla = RationalTchisla(n, target)
-    max_depth = tchisla.solve(max_depth)
-    rational_solution_list = tchisla.solution_prettyprint(target, True)
-    if rational_solution_list:
-        if solution_list:
-            print("=" * 20)
-        solution_list = rational_solution_list
-        for string in solution_list:
-            print(string)
+solvers = {
+    "integer": [
+        {"regex": integer_re, "constructor": int, "solver": IntegerTchisla}
+    ],
+    "rational": [
+        {"regex": rational_re, "constructor": Fraction, "solver": RationalTchisla}
+    ],
+    "incremental": [
+        {"regex": integer_re, "constructor": int, "solver": IntegerTchisla},
+        {"regex": rational_re, "constructor": Fraction, "solver": RationalTchisla}
+    ]
+}
 
 def general_solver(n, target, solver_type):
-    solver_map = {
-        "integer": integer_solver,
-        "rational": rational_solver,
-        "incremental": incremental_solver
-    }
-    solver_map[solver_type](n, target)
+    depth = None
+    solution = None
+    for solver in solvers[solver_type]:
+        if not solver["regex"].match(target):
+            continue
+        current_target = solver["constructor"](target)
+        tchisla = solver["solver"](n, current_target)
+        max_depth = depth
+        original_solution = solution
+        depth = tchisla.solve(max_depth)
+        solution = tchisla.solution_prettyprint(current_target, True)
+        if not solution:
+            depth = max_depth
+            solution = original_solution
+            continue
+        if original_solution:
+            print("=" * 20)
+        for string in solution:
+            print(string)
 
 def solve(string, solver_type):
     if "#" in string:
         array = string.split("#")
-        general_solver(int(array[1]), array[0], solver_type)
-    else:
+        target = array[0]
+        n = array[1]
+        if integer_re.match(n) and number_re.match(target):
+            n = int(n)
+            print(str(target) + " # " + str(n))
+            general_solver(n, target, solver_type)
+    elif number_re.match(string):
         for n in range(1, 10):
+            print(str(string) + " # " + str(n))
             general_solver(n, string, solver_type)
 
 def main(argv):
