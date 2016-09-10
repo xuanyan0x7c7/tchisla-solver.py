4405855340a70e1ef8ec102998a4a07c62bd318c
Yunqi Ouyang <oyyq9999999@163.com>
2016-09-11 02:43:26 +0800

    add support for ranges of targets and digits

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ integral_re = re.compile("^\\d+$")
 rational_re = re.compile("^\\d+(/\\d+)?$")
 number_re = rational_re
 
+problems_re = re.compile("^(?:\[(?P<targets>[^\]]*)\]|(?P<target>[1-9]\d*(?:\/[1-9]\d*)?))(?:#(?:\[(?P<digits>[^\]]*)\]|(?P<digit>[1-9])))?$")
+targets_re = re.compile("^(?:(?P<target>[1-9]\d*(?:\/[1-9]\d*)?)|(?P<start>[1-9]\d*)-(?P<end>[1-9]\d*))$")
+digits_re = re.compile("^(?P<first>[1-9])(?:-(?P<last>[1-9]))?$")
+
 solvers = {
     "integral": {
         "regex": integral_re, "constructor": int, "solver": IntegralTchisla
@@ -31,7 +35,7 @@ def general_solver(n, target, options):
     solution = None
     for solver_key in options.solvers:
         solver = solvers[solver_key]
-        if not solver["regex"].match(target):
+        if not solver["regex"].match(str(target)):
             continue
         current_target = solver["constructor"](target)
         tchisla = solver["solver"](n, current_target, verbose)
@@ -46,20 +50,77 @@ def general_solver(n, target, options):
         for string in solution:
             print(string)
 
-def solve(string, options):
-    if "#" in string:
-        array = string.split("#")
-        if len(array) == 2:
-            target = array[0]
-            n = array[1]
-            if integral_re.match(n) and number_re.match(target):
-                n = int(n)
-                print(str(target) + " # " + str(n))
-                general_solver(n, target, options)
-    elif number_re.match(string):
-        for n in range(1, 10):
-            print(str(string) + " # " + str(n))
-            general_solver(n, string, options)
+def solve(problem, options):
+    print(problem[0], '#', problem[1])
+    general_solver(problem[1], problem[0], options)
+
+def parse_problems(problems):
+    problem_list = []
+    for each in problems:
+        m = problems_re.match(each)
+        if not m:
+            continue
+
+        targets = m.group('targets')
+        targets = parse_targets(targets) if targets else [to_number(m.group('target'))]
+
+        digits = m.group('digits')
+        digit = m.group('digit')
+        digits = parse_digits(digits) if digits else [int(digit)] if digit else list(range(1, 10))
+
+        for target in targets:
+            for digit in digits:
+                problem_list.append((target, digit))
+    problem_list = sorted(set(problem_list))
+
+    return problem_list
+
+def to_number(string):
+    number = Fraction(string)
+    if number.denominator == 1:
+        number = int(number)
+    return number
+
+def parse_digits(digits):
+    digit_list = []
+    digits = digits.split(',')
+    for each in digits:
+        m = digits_re.match(each)
+        if not m:
+            continue
+        first = int(m.group('first'))
+        last = m.group('last')
+        if last:
+            last = int(last)
+        if not last:
+            last = first
+        if first > last:
+            first, last = last, first
+        digit_list = digit_list + list(range(first, last + 1))
+    digit_list = sorted(set(digit_list))
+    return digit_list
+
+
+def parse_targets(targets):
+    target_list = []
+    targets = targets.split(',')
+    for each in targets:
+        m = targets_re.match(each)
+        if not m:
+            continue
+        target = m.group('target')
+        if target:
+            target_list.append(to_number(target))
+            continue
+        start = int(m.group('start'))
+        end = int(m.group('end'))
+        if start > end:
+            start, end = end, start
+        target_list = target_list + list(range(start, end + 1))
+    target_list = sorted(set(target_list))
+    return target_list
+
+
 
 def main():
     default_solvers = ['integral', 'rational']
@@ -81,12 +142,12 @@ def main():
     )
     parser.add_argument('problem',
         nargs='+',
-        help='problem to solve'
+        help='problem to solve, examples: "2", "2#5", "[1,3]#8", "[2-4]#[6,7]", "[3-6,125,127]#[2-9]"'
     )
     options = parser.parse_args()
-    problem_list = options.problem
     if not options.solvers:
         options.solvers=default_solvers
+    problem_list = parse_problems(options.problem)
     for problem in problem_list:
         solve(problem, options)
 
