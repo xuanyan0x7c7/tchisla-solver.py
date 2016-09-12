c5b63d29bf9f8e315401e69c84619ec8cd0c61f5
Xuan Yan <xuanyan@xuanyan.ws>
2016-09-12 13:49:42 +0800

    use smaller threshold

--- solver/integral.py
+++ solver/integral.py
@@ -7,10 +7,10 @@ __all__ = ["IntegralTchisla"]
 
 class IntegralTchisla(BaseTchisla):
     constructor = int
-    MAX = 1 << 128
-    MAX_DIGITS = 128
-    MAX_CONCAT = 39
-    MAX_FACTORIAL = 34
+    MAX = 1 << 64
+    MAX_DIGITS = 64
+    MAX_CONCAT = 20
+    MAX_FACTORIAL = 20
 
     def __init__(self, n, target, verbose = False):
         super().__init__(n, target, verbose)
